db1ae9d6e2fa42e590384c808b048e37cfbed364
Chani <chanika@gmail.com>
2019-12-03 21:36:27 -0800

    part 1 actually correct

--- 2019-04.rb
+++ 2019-04.rb
@@ -6,19 +6,24 @@ max = data[1].to_i
 p "#{min} to #{max}"
 
 def valid(pass)
+    #p "trying #{pass}"
     foundDouble = false
     digits = pass.digits
     (0..4).each { |i|
-        if digits[i] > digits[i+1]
+        if digits[i] < digits[i+1]
+            #p "fail at i=#{i}"
             return false
         elsif digits[i] == digits[i+1]
+            #p "double at i=#{i}"
             foundDouble = true
         end
     }
+    #p "double? #{foundDouble}"
     return foundDouble
 end
 
 valids = (min..max).select {|pass| valid(pass)}
+#p valids
 p valids.length
 __END__
 265275-781584
