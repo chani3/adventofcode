2b2c5dda3f64f0e2572989934e22d78a50cebbfa
Chani <chanika@gmail.com>
2019-12-08 17:02:53 -0800

    day 7 part 1 works
    but the code is getting ugly. wanna refactor it into a class once I've
    read part 2.

---
+++ 2019-07.rb
@@ -0,0 +1,100 @@
+#!/usr/bin/ruby
+
+sData=DATA.readlines[0].split(',').map(&:to_i)
+#io is suuuch a hack atm
+$output = 0
+$input = []
+def initInput(values)
+    $input = values
+end
+def getInput
+    return $input.shift
+end
+
+def runCode(data, input)
+opSum = { params: 3, ret: :value, run: Proc.new { |a, b| a + b } }
+opMult = { params: 3, ret: :value, run: Proc.new{ |a, b| a * b } }
+opInput = { params: 1, ret: :value, run: Proc.new{ getInput }}
+opOutput = { params: 1, run: Proc.new{ |o| $output = o[0] }}
+opEnd = { params: 0, run: Proc.new{ return }}
+opJumpT = { params: 2, ret: :jump, run: Proc.new{ |test, p|
+    if test != 0
+        p
+    else
+        -1
+    end
+}}
+opJumpF = { params: 2, ret: :jump, run: Proc.new{ |test, p|
+    if test == 0
+        p
+    else
+        -1
+    end
+}}
+opLess = { params: 3, ret: :value, run: Proc.new{ |a, b| a < b ? 1 : 0}}
+opEqual = { params: 3, ret: :value, run: Proc.new{ |a, b| a == b ? 1 : 0}}
+
+opCodes = { 1 => opSum, 2 => opMult, 3 => opInput, 4 => opOutput,
+            5 => opJumpT, 6 => opJumpF, 7 => opLess, 8 => opEqual,
+            9 => opEnd }
+
+    initInput(input)
+    ip = 0
+    loop do
+        opField = data[ip].digits
+        opCode = opField[0]
+        paramModes = opField.slice(2,3)
+        paramAddrs = []
+        op = opCodes[opCode]
+        #p "opfield is #{opField}"
+        #p "op is #{op}"
+        #p "pM is #{paramModes}"
+        #get params
+        op[:params].times { |i|
+            ip+=1
+            if paramModes && paramModes[i] == 1
+                paramAddrs[i] = ip
+                #p "direct mode #{i}"
+            else
+                paramAddrs[i] = data[ip]
+                #p "normal mode #{i}"
+            end
+        }
+        ip += 1
+        #p paramAddrs
+        params = paramAddrs.map { |addr| data[addr] }
+        #p params
+        ret = op[:run].call(params)
+        #p "returned #{ret}"
+        if (op[:ret] == :value)
+            data[paramAddrs.last] = ret
+        elsif op[:ret] == :jump && ret >= 0
+            ip = ret
+        end
+    end
+end
+
+def trySequence(seq, data)
+    p "trying #{seq}"
+    acc = 0
+    seq.each { |setting|
+        runCode(data.dup, [setting, acc])
+        acc = $output
+    }
+    return acc
+end
+
+p "hello"
+maxSignal = 0
+
+(0..4).to_a.permutation { |p|
+    signal = trySequence(p, sData)
+    if signal > maxSignal
+        maxSignal = signal
+    end
+}
+
+p maxSignal
+
+__END__
+3,8,1001,8,10,8,105,1,0,0,21,34,51,76,101,114,195,276,357,438,99999,3,9,1001,9,3,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,102,4,9,9,1001,9,5,9,4,9,99,3,9,1002,9,4,9,101,3,9,9,102,5,9,9,1001,9,2,9,1002,9,2,9,4,9,99,3,9,1001,9,3,9,102,2,9,9,101,4,9,9,102,3,9,9,101,2,9,9,4,9,99,3,9,102,2,9,9,101,4,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,99
