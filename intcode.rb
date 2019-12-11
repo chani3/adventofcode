4089e279a945536aa4f97bd9d751c58ad46b1c7a
Chani <chanika@gmail.com>
2019-12-10 16:11:06 -0800

    Separate file for threadsafe intcode
    it's only missing day 9's features

---
+++ intcode.rb
@@ -0,0 +1,99 @@
+
+class IntCode
+    def getInput
+        p "pulling from #{@inQ}"
+        return @inQ.pop
+    end
+    def output(o)
+        @outQ << o
+    end
+    def sum(a, b)
+        a + b
+    end
+    def mult(a, b)
+        a*b
+    end
+    opSum = { params: 3, ret: :value, run: :sum }
+    opMult = { params: 3, ret: :value, run: :mult }
+    opInput = { params: 1, ret: :value, run: :getInput }
+    opOutput = { params: 1, run: :output }
+    opEnd = { params: 0, ret: :exit } #fffff return broke
+    opJumpT = { params: 2, ret: :jump, run: :jumpT }
+    def jumpT(test, p)
+        if test != 0
+            p
+        else
+            -1
+        end
+    end
+    opJumpF = { params: 2, ret: :jump, run: :jumpF }
+    def jumpF(test, p)
+        if test == 0
+            p
+        else
+            -1
+        end
+    end
+    opLess = { params: 3, ret: :value, run: :less }
+    def less(a, b)
+        a < b ? 1 : 0
+    end
+    opEqual = { params: 3, ret: :value, run: :equal }
+    def equal(a, b)
+        a == b ? 1 : 0
+    end
+
+    OpCodes = { 1 => opSum, 2 => opMult, 3 => opInput, 4 => opOutput,
+            5 => opJumpT, 6 => opJumpF, 7 => opLess, 8 => opEqual,
+            9 => opEnd }
+
+    def initialize(data, inputQueue, outputQueue)
+        @data = data.dup
+        @inQ = inputQueue
+        @outQ = outputQueue
+    end
+    def run()
+        ip = 0
+        loop do
+            opField = @data[ip].digits
+            opCode = opField[0]
+            paramModes = opField.slice(2,3)
+            paramAddrs = []
+            op = OpCodes[opCode]
+            #p "opfield is #{opField}"
+            #p "op is #{op}"
+            if op[:ret] == :exit
+                return
+            end
+
+            #p "pM is #{paramModes}"
+            #get params
+            op[:params].times { |i|
+                ip+=1
+                if paramModes && paramModes[i] == 1
+                    paramAddrs[i] = ip
+                    #p "direct mode #{i}"
+                else
+                    paramAddrs[i] = @data[ip]
+                    #p "normal mode #{i}"
+                end
+            }
+            ip += 1
+            #p paramAddrs
+            params = paramAddrs.map { |addr| @data[addr] }
+            #have to get the exact right # of args now
+            if (op[:ret] == :value)
+                params.pop
+            end
+            #p params
+            ret = self.send(op[:run], *params)
+            #p "returned #{ret}"
+            if (op[:ret] == :value)
+                @data[paramAddrs.last] = ret
+            elsif op[:ret] == :jump && ret >= 0
+                ip = ret
+            end
+        end
+    end
+end
+
