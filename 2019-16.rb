80d870d6e635e2125a40ce504a33a7843b544aae
Chani <chanika@gmail.com>
2019-12-15 22:33:44 -0800

    saving patterns to a file
    this looks doomed, I'm fifty-something in and the file is half a gig.
    maybe I should drop the pattern arrays entirely.

--- 2019-16.rb
+++ 2019-16.rb
@@ -13,15 +13,34 @@ end
 list = data[0].chomp.split('').map(&:to_i)
 offset = data[0][0..6].to_i
 
-basePattern = [0, 1, 0, -1]
+#fuuuck this is gonna be slow
+list = list * 10000
+
+smallBasePattern = [0, 1, 0, -1]
+basePattern = smallBasePattern * (list.length/4 + 2)
 
 $patterns = {}
+pData = nil
+$pDataFile = "2019-16.patterns"
+File.open($pDataFile, "r") { |file|
+    pData = file.readlines
+}
+pData.each.with_index { |line, i|
+    $patterns[i] = line.split(',').map(&:to_i)
+}
 
 def generatePattern(basePattern, pos, length)
     if ! $patterns.has_key? pos
+        p "making pattern #{pos}"
+        div = pos+1
         $patterns[pos] = Array.new(length) { |i|
-            basePattern[((i+1)/(pos+1))%4]
+            basePattern[((i+1)/div)]
         }
+        File.open($pDataFile, "a") { |file|
+            file.puts $patterns[pos].join(",")
+        }
+
+        p "made pattern"
     end
     $patterns[pos]
 end
@@ -30,7 +49,11 @@ def applyPatternToIndex(basePattern, list, i)
     p "#{i}"
     pattern = generatePattern(basePattern, i, list.length)
     #p "pattern for #{i}: #{pattern}"
-    value = list.zip(pattern).sum(0) { |e| e[0] * e[1] }
+    value = 0
+    list.each.with_index { |e, i|
+        #p "adding #{e} * #{i}"
+        value += e * pattern[i]
+    }
     value.abs % 10
 end
 
@@ -41,12 +64,9 @@ def applyPatternToList(basePattern, list)
     }
 end
 
-#fuuuck this is gonna be slow
-list = list * 10000
-
 p offset
 100.times { list = applyPatternToList(basePattern, list) }
-p list[0, 8]
+p list[offset, 8]
 
 __END__
 59773775883217736423759431065821647306353420453506194937477909478357279250527959717515453593953697526882172199401149893789300695782513381578519607082690498042922082853622468730359031443128253024761190886248093463388723595869794965934425375464188783095560858698959059899665146868388800302242666479679987279787144346712262803568907779621609528347260035619258134850854741360515089631116920328622677237196915348865412336221562817250057035898092525020837239100456855355496177944747496249192354750965666437121797601987523473707071258599440525572142300549600825381432815592726865051526418740875442413571535945830954724825314675166862626566783107780527347044
