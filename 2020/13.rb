d36f3a0a47ef744eeee754ede797538deb8b79b1
Chani <chanika@gmail.com>
2020-12-12 21:01:58 -0800

    problem 13

---
+++ 2020/13.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+require_relative "../helpers"
+data = Helpers.loadData
+
+p data[0]
+
+__END__
+1000677
+29,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,661,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,x,x,x,x,23,x,x,x,x,x,x,x,521,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,19
