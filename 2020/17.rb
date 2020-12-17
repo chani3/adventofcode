6f607c55050afa9c0fbeb7c4553be7b57611134e
Chani <chanika@gmail.com>
2020-12-16 22:53:07 -0800

    part 2
    that was... weirdly easy.

--- 2020/17.rb
+++ 2020/17.rb
@@ -12,33 +12,33 @@ Height = Buffer + data.size
 Depth = Buffer + 1
 
 def newPocketArray
-  Array.new(Width) { Array.new(Height) { Array.new(Depth, Inactive) }}
+  Array.new(Width) { Array.new(Height) { Array.new(Depth) { Array.new(Depth, Inactive) }}}
 end
 pocket = newPocketArray
 #p pocket
 data.each_with_index { |line, i|
   line.chars.each_with_index { |char, j|
     #p "setting #{j+Offset} #{i+Offset} #{Offset} to #{char}"
-    pocket[j+Offset][i+Offset][Offset] = char
+    pocket[j+Offset][i+Offset][Offset][Offset] = char
     #p pocket[j+Offset][i+Offset][Offset]
   }
 }
 p pocket[9][14][7]
 
 Directions = [-1, 0, 1]
-Neighbours = Directions.product(Directions, Directions).reject { |a| a == [0,0,0] }
+Neighbours = Directions.product(Directions, Directions, Directions).reject { |a| a == [0,0,0,0] }
 def invalid(pos)
-  pos[0] < 0 or pos[1] < 0 or pos[2] < 0 or pos[0] >= Width or pos[1] >= Height or pos[2] >= Depth
+  pos[0] < 0 or pos[1] < 0 or pos[2] < 0 or pos[3] < 0 or pos[0] >= Width or pos[1] >= Height or pos[2] >= Depth or pos[3] >= Depth
 end
 def getState(pos, pocket)
   if invalid(pos)
     return Inactive
   end
-  pocket[pos[0]][pos[1]][pos[2]]
+  pocket[pos[0]][pos[1]][pos[2]][pos[3]]
 end
 def getNextState(pos, pocket)
   oldState = getState(pos, pocket)
-  myNeighbours = Neighbours.map { |rel| [pos[0]+rel[0],pos[1]+rel[1],pos[2]+rel[2]] }
+  myNeighbours = Neighbours.map { |rel| [pos[0]+rel[0],pos[1]+rel[1],pos[2]+rel[2],pos[3]+rel[3]] }
   sum = myNeighbours.sum { |npos|
     nState = getState(npos, pocket)
     nState == Active ? 1 : 0
@@ -50,10 +50,12 @@ def getNextState(pos, pocket)
 end
 
 def sumPocket(pocket)
-  pocket.sum { |sheet|
-    sheet.sum { |line|
-      line.sum { |point|
-        point == Active ? 1 : 0
+  pocket.sum { |cube|
+    cube.sum { |sheet|
+      sheet.sum { |line|
+        line.sum { |point|
+          point == Active ? 1 : 0
+        }
       }
     }
   }
@@ -65,7 +67,9 @@ p sumPocket(pocket)
   (0..Width-1).each { |x|
     (0..Height-1).each { |y|
       (0..Depth-1).each { |z|
-        nextPocket[x][y][z] = getNextState([x,y,z], pocket)
+        (0..Depth-1).each { |w|
+          nextPocket[x][y][z][w] = getNextState([x,y,z,w], pocket)
+        }
       }
     }
   }
